c79168336d65501d059ca2ba23100862bf3b0116
Andrey Kislov <andrej.kislov@spellabs.ru>
2019-04-02 12:24:52 +0300

    Fix mqtt broker

--- src/services/constants.js
+++ src/services/constants.js
@@ -3,6 +3,6 @@ export const CONSTANTS = {
     MESSAGING_SENDER_ID: '934191126799',
     VK_CLIENT_ID: '6809010',
 
-    MQTT_SERVER_URL: 'ws://test.mosquitto.org:8080',
+    MQTT_SERVER_URL: 'wss://test.mosquitto.org:8081',
     MQTT_TOPIC: 'Topic_1'
 };
